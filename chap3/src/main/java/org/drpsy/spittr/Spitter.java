9b940f73276b4d3b63f102b84bac0437f7e9ba88
Mikhail Gashenko <dr.psycho@live.com>
2017-11-22 23:30:34 +0600

    Spring on the Web: Validating forms

--- chap3/src/main/java/org/drpsy/spittr/Spitter.java
+++ chap3/src/main/java/org/drpsy/spittr/Spitter.java
@@ -1,5 +1,7 @@
 package org.drpsy.spittr;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 
@@ -9,9 +11,21 @@ import org.apache.commons.lang3.builder.HashCodeBuilder;
 public class Spitter {
 
   private Long id;
+
+  @NotNull
+  @Size(min = 2, max = 30)
   private String firstName;
+
+  @NotNull
+  @Size(min = 2, max = 30)
   private String lastName;
+
+  @NotNull
+  @Size(min = 5, max = 16)
   private String userName;
+
+  @NotNull
+  @Size(min = 5, max = 25)
   private String password;
 
   public Spitter() {}
--- chap3/src/main/java/org/drpsy/spittr/web/SpitterController.java
+++ chap3/src/main/java/org/drpsy/spittr/web/SpitterController.java
@@ -3,11 +3,13 @@ package org.drpsy.spittr.web;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
+import javax.validation.Valid;
 import org.drpsy.spittr.Spitter;
 import org.drpsy.spittr.data.SpitterRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -28,20 +30,27 @@ public class SpitterController {
     this.spitterRepository = spitterRepository;
   }
 
-  @RequestMapping(value = "/register", method = GET)
-  public String showRegistrationForm() {
-    return "registerForm";
-  }
-
   @RequestMapping(value = "/{userName}", method = GET)
-  public String showSpitterPorfile(@PathVariable String userName, Model model) {
+  public String showSpitterProfile(@PathVariable String userName, Model model) {
     Spitter spitter = spitterRepository.findByUserName(userName);
     model.addAttribute(spitter);
     return "profile";
   }
 
+  @RequestMapping(value = "/register", method = GET)
+  public String showRegistrationForm() {
+    return "registerForm";
+  }
+
+  // This object has firstName, lastName, username, and password properties that
+  // will be populated from the request parameters of the same name.
   @RequestMapping(value = "/register", method = POST)
-  public String processRegistration(Spitter spitter) {
+  public String processRegistration(@Valid Spitter spitter, Errors errors) {
+
+    if (errors.hasErrors()) {
+      return "registerForm";  // Return to form on validation errors.
+    }
+
     spitterRepository.save(spitter);
     return "redirect:/spitter/" + spitter.getUserName();
   }
