1b15f05bd7e35d2faec7791fef540b7de6ede822
Mikhail Gashenko <dr.psycho@live.com>
2017-12-29 02:12:37 +0600

    Securing Web Applications: Logout POST-link fix

--- chap5/src/main/java/org/drpsy/spittr/config/SecurityConfig.java
+++ chap5/src/main/java/org/drpsy/spittr/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -60,13 +61,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         // Permitted.
         .antMatchers("/", "/home").permitAll()
         .antMatchers("/spittr/register").permitAll()
-        .antMatchers("/spittr/auth/**").permitAll()
+        .antMatchers(HttpMethod.GET, "/spittr/auth/**").denyAll()
+        .antMatchers(HttpMethod.POST,"/spittr/auth/**").permitAll()
+
         // Authenticated.
         .antMatchers("/spittles/**").authenticated()
+
         // Has role.
         .antMatchers("/admin/**").hasRole("ADMIN")
         .antMatchers("/spittr/**").hasRole("SPITTR")
 
+        // Everything else.
         .anyRequest().permitAll();
 
   }
